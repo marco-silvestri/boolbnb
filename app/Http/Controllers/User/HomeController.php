e22826a003002f771972c6bc2e716708cf5f24e0
marco-silvestri <m.silvestri03@gmail.com>
2020-07-09 10:54:23 +0200

    CRUD Created

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -7,8 +7,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         return view('user.welcome');
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/carmine/Downloads/Corso Boolean #12/esercizi-giugno/boolbnb/boolbnb/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/carmine/Downloads/Corso Boolean #12/esercizi-giugno/boolbnb/boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/msilvestri/Desktop/Code/boolbnb-team5/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/msilvestri/Desktop/Code/boolbnb-team5/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
