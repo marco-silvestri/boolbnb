279377410ec307703fefa2ffa9586b26d76b3611
marco-silvestri <m.silvestri03@gmail.com>
2020-07-09 15:48:31 +0200

    Iterable maps included

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,18 +1,14 @@
 @extends('layouts.app')
-
+@include('shared.components.Leaflet-include')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-       
         <h1>Questa sarà la index</h1>
     </div>
-
-
     <div class="container">
-        @dump($apartments)
+        @foreach ($apartments as $apartment)
+            @include('shared.components.Card')
+        @endforeach
     </div>
 </div>
-
-
-
 @endsection
