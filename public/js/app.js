573b71018ca11e4d926e033fb17c0a4b5c58aac4
marco-silvestri <m.silvestri03@gmail.com>
2020-07-09 23:23:38 +0200

    Added conditional-on-user cards, refactored card component

--- public/js/app.js
+++ public/js/app.js
@@ -47760,8 +47760,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Vincenzo\Desktop\PROGETTO FINALE\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Vincenzo\Desktop\PROGETTO FINALE\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/msilvestri/Desktop/Code/boolbnb-team5/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/msilvestri/Desktop/Code/boolbnb-team5/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ }),
