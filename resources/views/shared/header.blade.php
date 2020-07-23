ff16634a612b7770eb1b1ce9a07950c145ad19f1
marco-silvestri <m.silvestri03@gmail.com>
2020-07-23 10:44:13 +0200

    Added SASS vars and typography

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -11,8 +11,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet"> 
     
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
