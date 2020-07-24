1989928badfaf3047b121436848b85cb59639509
Vincenzo Laveneziana <laveneziana.vincenzo@gmail.com>
2020-07-24 17:23:24 +0200

    fix mixin scss

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,46 +1,48 @@
 require('./bootstrap');
 import 'places.js';
 
+
 $(document).ready(function () {
     
+    //SCROLL NAVBAR
     $("#app").toggleClass('scrolled');
 
     $(window).scroll(function(){
         $("#app").toggleClass('scrolled', $(this).scrollTop() > 150)
     })
 
-    // C A R O U S E L //
-var first = $ (".first");
-var last = $(".last");
-var left = $ (".fa-caret-left");
-var right = $ (".fa-caret-right");
+        // C A R O U S E L //
+    var first = $ (".first");
+    var last = $(".last");
+    var left = $ (".fa-caret-left");
+    var right = $ (".fa-caret-right");
 
-right.click(function(){
-    var current = $(".discover");
-    current.removeClass("discover");
-    current.addClass("discover-none");
+    right.click(function(){
+        var current = $(".discover");
+        current.removeClass("discover");
+        current.addClass("discover-none");
 
-    current.next().removeClass("discover-none");
-    current.next().addClass("discover");
+        current.next().removeClass("discover-none");
+        current.next().addClass("discover");
 
-    if(current.hasClass("last")){
-        first.removeClass("discover-none");
-        first.addClass("discover");
-      }
-});
+        if(current.hasClass("last")){
+            first.removeClass("discover-none");
+            first.addClass("discover");
+        }
+    });
 
-left.click(function(){
-    var current = $(".discover");
-    current.removeClass("discover");
-    current.addClass("discover-none");
+    left.click(function(){
+        var current = $(".discover");
+        current.removeClass("discover");
+        current.addClass("discover-none");
 
-    current.prev().removeClass("discover-none");
-    current.prev().addClass("discover");
+        current.prev().removeClass("discover-none");
+        current.prev().addClass("discover");
 
-    if(current.hasClass("first")){
-        last.removeClass("discover-none");
-        last.addClass("discover");
-      }
-});
+        if(current.hasClass("first")){
+            last.removeClass("discover-none");
+            last.addClass("discover");
+        }
+    });
 });
 
